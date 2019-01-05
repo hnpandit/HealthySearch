e3d372087fb050d6a4ec5a5b3b10f457b718f4d9
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-01-05 07:33:19 -0500

    added UI for maps

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -7,10 +7,18 @@
 // App Key - w7UvEiaWJksvqgcOea4n
 // App Code - HqRyRkE_CPdtcF-Qo5lXdA
 
-function displayMap(lng, lat)
+// Global Variables
+var platform;
+var maptypes;
+var map;
+
+var longitude;
+var latitude;
+
+function displayMap()
 {
     // Initialize platform
-    var platform = new H.service.Platform({
+    platform = new H.service.Platform({
     'app_id': 'w7UvEiaWJksvqgcOea4n',
     'app_code': 'HqRyRkE_CPdtcF-Qo5lXdA',
     'useHTTPS': true,
@@ -18,16 +26,31 @@ function displayMap(lng, lat)
     });
 
     // Obtain the default map types from the platform object
-    var maptypes = platform.createDefaultLayers();
+    maptypes = platform.createDefaultLayers();
 
     // Instantiate (and display) a map object:
-    var map = new H.Map(
+    map = new H.Map (
     document.getElementById('mapContainer'),
     maptypes.normal.map,
     {
       zoom: 10,
-      center: { lng: -73.935242, lat: 40.730610 }
+      center: { lng: longitude, lat: latitude }
     });
 }
 
-displayMap(1, 1);
+$("#showMap").on("click", function(event)
+{
+  event.preventDefault();
+
+  // Get User Input
+  longitude = $("#txtLongitude").val().trim();
+  latitude = $("#txtLatitude").val().trim();
+
+  console.log("Longitude:" + longitude);
+  console.log("Latitude:" + latitude);
+  
+  $("#mapContainer").empty();
+  $("#txtLongitude").val('');
+  $("#txtLatitude").val('');
+  displayMap();
+})
