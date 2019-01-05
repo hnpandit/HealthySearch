32e6094f2ed7f5957f7e92bfe4a83a5e422d00b3
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-01-05 09:42:51 -0500

    demo ready

--- assets/javascript/db.js
+++ assets/javascript/db.js
@@ -20,6 +20,8 @@ firebase.initializeApp(config);
 
 database = firebase.database();
 
+//database.ref("/data").set("Test");  
+
 var user = {
       userName:"",
       userFirstName:"",
@@ -56,8 +58,11 @@ function addFavorite(userName, Doctor, DoctorUID)
   database.ref("/favorites").push(favorites);  
 }
 
-function populateData()
+function initialzeData()
 {
+    //database.ref("/users").empty();
+    //database.ref("/favorites").empty();
+
     addUser("hpandit", "Himanshu", "Pandit", "hp@rcb.com", "(111) 111-1111", "08816");
     addUser("icohen", "Ilene", "Cohen", "ic@rcb.com", "(222) 222-2222", "07608");
     addUser("dsires", "Dan", "Sires", "ds@rcb.com", "(333) 333-3333", "90210");
@@ -69,15 +74,52 @@ function populateData()
     addFavorite("dsires", "Dr. John Doe", "5bd1d56611167f74712548bbb968e9d8");
     addFavorite("jduran", "Dr. Miin Mathew", "5bd1d56611167f74712548bbb968e9d8"); 
 
-    var newKey = firebase.database().ref().child('favorites').push().key;
-    console.log(newKey);
+    //var newKey = firebase.database().ref().child('favorites').push().key;
 
-    favorites.userName = "hpandit";
-    favorites.doctorName = "New Doctor";
-    favorites.DoctorUID = "SomeUID";
-    var updates = {};
-    updates['/favorites/' + newKey + '/'] = favorites;
-    database.ref().update(updates);
+    //favorites.userName = "hpandit";
+    //favorites.doctorName = "New Doctor";
+    //favorites.DoctorUID = "SomeUID";
+    //var updates = {};
+    //updates['/favorites/' + newKey + '/'] = favorites;
+    //database.ref().update(updates);
+}
+
+function displayUsers()
+{
+    var ref = firebase.database().ref("/users");
+
+    ref.on("value", function(snapshot) {
+      
+      var users = [];
+      users = snapshotToArray(snapshot);
+      $("#dataContainer").append("Displaying Users" + "<br>");
+      for (i=0; i<users.length; i++)
+      {
+        $("#dataContainer").append(users[i].userFirstName);
+        $("#dataContainer").append("<br>");
+      }
+      }, function (error) {
+        console.log("Error: " + error.code);
+    });
+}
+
+function displayFavorites()
+{
+    var ref = firebase.database().ref("/favorites");
+
+    ref.on("value", function(snapshot) {
+      
+      var favorites = [];
+      favorites = snapshotToArray(snapshot);
+      $("#dataContainer").append("Displaying User Favorites" + "<br>");
+      for (i=0; i<favorites.length; i++)
+      {
+        $("#dataContainer").append(favorites[i].userName + "(" + favorites[i].doctorName + ")");
+        $("#dataContainer").append("<br>");
+      }
+      }, function (error) {
+        console.log("Error: " + error.code);
+    });
 }
 
 function snapshotToArray(snapshot) {
@@ -93,24 +135,14 @@ function snapshotToArray(snapshot) {
   return returnArr;
 };
 
-function getUserZip()
+$("#initializeDatabase").on("click", function(event)
 {
+  $("#dataContainer").empty();
+  displayUsers();
+  displayFavorites();
+})
 
-  var ref = firebase.database().ref("/users");
-
-  ref.on("value", function(snapshot) {
-     var users = [];
-     users = snapshotToArray(snapshot);
-     for (i=0; i<users.length; i++)
-     {
-      console.log(users[i].userName);
-      console.log(users[i].key);
-      database.ref("/users/" + users[i].key).remove();
-     }
-  }, function (error) {
-     console.log("Error: " + error.code);
-  });
-}
-
-getUserZip();
-populateData();
+$("#populateDatabase").on("click", function(event)
+{
+  initialzeData();
+})
