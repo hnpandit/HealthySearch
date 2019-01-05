d585b395728d660eb6d6abd6906ce6d8f6d7d6cd
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-01-05 06:02:03 -0500

    near final version

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -6,3 +6,26 @@
 // Initialize Firebase
 // App Key - w7UvEiaWJksvqgcOea4n
 // App Code - HqRyRkE_CPdtcF-Qo5lXdA
+
+function displayMap(lng, lat)
+{
+    // Initialize platform
+    var platform = new H.service.Platform({
+    'app_id': 'w7UvEiaWJksvqgcOea4n',
+    'app_code': 'HqRyRkE_CPdtcF-Qo5lXdA'
+    });
+
+    // Obtain the default map types from the platform object
+    var maptypes = platform.createDefaultLayers();
+
+    // Instantiate (and display) a map object:
+    var map = new H.Map(
+    document.getElementById('mapContainer'),
+    maptypes.normal.map,
+    {
+      zoom: 10,
+      center: { lng: -73.935242, lat: 40.730610 }
+    });
+}
+
+displayMap(1, 1);
--- assets/javascript/search.js
+++
@@ -1,6 +0,0 @@
-// Rutgers Coding BootCamp - Full Stack Developer - Mon/Wed
-// Project 1 - HealthySearch Ilene, Dan, Juan & Himanshu  
-// search.js - to interface with medical information search APIs
-// January 12, 2019
-    
-// Initialize Firebase
